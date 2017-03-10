573505308555b97955288ae129151cd945fd6b55
flabbergast <flabbergast@runbox.com>
2017-03-10 19:40:07 +0000

    Update to current tmk_keyboard/tmk_core code.

--- kb45p/keymap_plain.c
+++ kb45p/keymap_plain.c
@@ -24,7 +24,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
-const uint16_t fn_actions[] = {
+const action_t fn_actions[] = {
   [0] = ACTION_LAYER_MOMENTARY(1),
   [1] = ACTION_LAYER_MOMENTARY(2),
 };
